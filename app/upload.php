286eb04c82ab535fb22c069e29871e1695eb700e
Gerard Falco <gerardfp@users.noreply.github.com>
2021-12-20 18:07:05 +0100

    Update upload.php

--- app/upload.php
+++ app/upload.php
@@ -6,7 +6,7 @@ if (!empty($_POST["post"])) {
         $photoid = uniqid();
         move_uploaded_file($_FILES['photo']['tmp_name'], 'uploads/' . $photoid);
     }
-    $db = new mysqli("db.extagram.edt", "extagram_admin", "pass123", "extagram_db");
+    $db = new mysqli("s6", "extagram_admin", "pass123", "extagram_db");
     $stmt = $db->prepare("INSERT INTO posts VALUES(?,?)");
     $stmt->bind_param("ss", $_POST["post"], $photoid);
     $stmt->execute();
